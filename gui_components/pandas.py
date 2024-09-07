0dd2d6c2d762579a0806eade88bc258b50f71d55
Matthew <28072641+matthewacalder@users.noreply.github.com>
2024-09-07 22:33:00 +0000

    Add filters

---
+++ gui_components/pandas.py
@@ -0,0 +1,131 @@
+import pandas as pd
+import streamlit as st
+
+from pandas.api.types import (
+    is_categorical_dtype,
+    is_datetime64_any_dtype,
+    is_numeric_dtype,
+    is_object_dtype,
+)
+from pandas._libs.tslibs.parsing import DateParseError
+from streamlit.delta_generator import DeltaGenerator
+
+
+def _apply_categorical_filter(
+    df: pd.DataFrame, col_name: str, gui_column: DeltaGenerator
+) -> pd.DataFrame:
+    user_cat_input = gui_column.multiselect(
+        f"Values for {col_name}",
+        df[col_name].unique(),
+        default=list(df[col_name].unique()),
+    )
+    return df[df[col_name].isin(user_cat_input)]
+
+def _apply_numerical_filter(
+    df: pd.DataFrame, col_name: str, gui_column: DeltaGenerator
+) -> pd.DataFrame:
+    _min = float(df[col_name].min())
+    _max = float(df[col_name].max())
+    step = (_max - _min) / 100
+    user_num_input = gui_column.slider(
+        f"Values for {col_name}",
+        min_value=_min,
+        max_value=_max,
+        value=(_min, _max),
+        step=step,
+    )
+    return df[df[col_name].between(*user_num_input)]
+
+def _apply_datetime_filter(
+    df: pd.DataFrame, col_name: str, gui_column: DeltaGenerator
+) -> pd.DataFrame:
+    user_date_input = gui_column.date_input(
+        f"Values for {col_name}",
+        value=(
+            df[col_name].min(),
+            df[col_name].max(),
+        ),
+    )
+    if len(user_date_input) == 2:
+        user_date_input = tuple(map(pd.to_datetime, user_date_input))
+        start_date, end_date = user_date_input
+        return df.loc[df[col_name].between(start_date, end_date)]
+    return df
+
+def _apply_regex_filter(
+    df: pd.DataFrame, col_name: str, gui_column: DeltaGenerator
+) -> pd.DataFrame:
+    user_text_input = gui_column.text_input(
+        f"Substring or regex in {col_name}",
+    )
+    if user_text_input:
+        return df[df[col_name].astype(str).str.contains(user_text_input)]
+    return df
+
+def _filter_column_contents(df: pd.DataFrame, filter_cols: list[str]) -> pd.DataFrame:
+    to_filter_columns = st.multiselect("Filter dataframe on", filter_cols)
+    _, right = st.columns((1, 20))
+    for column in to_filter_columns:
+        # Treat columns with < 10 unique values as categorical
+        if is_categorical_dtype(df[column]) or df[column].nunique() < 10:
+            df = _apply_categorical_filter(df, column, right)
+        elif is_numeric_dtype(df[column]):
+            df = _apply_numerical_filter(df, column, right)
+        elif is_datetime64_any_dtype(df[column]):
+            df = _apply_datetime_filter(df, column, right)
+        else:
+            df = _apply_regex_filter(df, column, right)
+    return df
+
+
+def _filter_columns(df: pd.DataFrame) -> pd.DataFrame:
+    all_columns = df.columns
+    options = st.multiselect(
+        label = "Columns to Show:",
+        options=all_columns,
+        default=all_columns,
+    )
+    df = df.drop(columns=set(all_columns) - set(options))
+    return df
+
+
+def filter_dataframe(df: pd.DataFrame, no_filter_cols: list[str]) -> pd.DataFrame:
+    """
+    Adds a UI on top of a dataframe to let viewers filter columns
+    A lot of code taken from:
+        https://blog.streamlit.io/auto-generate-a-dataframe-filtering-ui-in-streamlit-with-filter_dataframe/
+
+    Args:
+        df (pd.DataFrame): Original dataframe
+
+    Returns:
+        pd.DataFrame: Filtered dataframe
+    """
+    # Only show modification options if selected
+    modify = st.checkbox("Add filters")
+    if not modify:
+        return df
+
+    # Configure what column contents can be controlled
+    df = df.copy()
+    filter_cols = list(df.columns)
+    for col in no_filter_cols:
+        filter_cols.remove(col)
+
+    # Try to convert datetimes into a standard format (datetime, no timezone)
+    for col in filter_cols:
+        if is_object_dtype(df[col]):
+            try:
+                df[col] = pd.to_datetime(df[col])
+            except DateParseError:
+                pass
+        if is_datetime64_any_dtype(df[col]):
+            df[col] = df[col].dt.tz_localize(None)
+
+    # Apply filters
+    modification_container = st.container()
+    with modification_container:
+        df = _filter_column_contents(df, filter_cols)
+        df = _filter_columns(df)
+
+    return df
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import streamlit as st
 
+from gui_components.pandas import filter_dataframe
+
 def main() -> None:
-    st.title("Trackmania Campaign Author Time Difficulty")
+    st.title("Trackmania 2020 Campaign Author Time Difficulty")
     st.markdown(
         "The below table shows the author times for all Trackmania 2020 Campaign tracks (as of 7th September 2024) "
         "alongisde world record times and the 10,000th position on leaderboard times."
@@ -11,15 +13,16 @@ def main() -> None:
         "It is suggested that filtering from high to low on `10k Time Difference` or "
         "`10k Time % Difference` column gives a good indication of easy -> hard author times."
     )
-    st.markdown("All data taken from https://webservices.openplanet.dev/ on 7th September 2024.")
 
-    df = pd.read_csv("campaign_data.csv")
+    df = pd.read_csv("data_ingestion/campaign_data.csv")
+
     st.data_editor(
-        df,
+        filter_dataframe(df, no_filter_cols=["Thumbnail", "Completed"]),
         column_config={"Thumbnail": st.column_config.ImageColumn("")},
-        hide_index=True,    
+        hide_index=True,
     )
 
+    st.markdown("All data taken from https://webservices.openplanet.dev/ on 7th September 2024.")
     st.markdown(
         "The API used to obtain data only returns first 10,000 track times per track; an extension to determine the "
         "number/percentage of users to obtain an author medal on each track is suggested, if a data source is found."
