60d8176ca0d323a6c0d0bba0b115f50866537b2a
Matthew <28072641+matthewacalder@users.noreply.github.com>
2024-09-07 17:38:46 +0000

    Initial app build

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,24 @@
+import pandas as pd
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+def main() -> None:
+    st.title("Trackmania Campaign Author Time Difficulty")
+    st.markdown("All data taken from https://webservices.openplanet.dev/ on 7th September 2024.")
+    st.markdown(
+        "It is suggested that filtering from high to low on `10k Time % Difference` or "
+        "`10k Time % Difference` column gives a good indication of easy -> hard author times."
+    )
+    st.markdown(
+        "The above API only returns first 10,000 track times per track; an extension to determine the "
+        "number/percentage of users to obtain an author medal on each track is suggested, if a data source is found."
+    )
+
+    df = pd.read_csv("campaign_data.csv")
+    st.data_editor(
+        df,
+        column_config={"Thumbnail": st.column_config.ImageColumn("")},
+        hide_index=True,    
+    )
+
+if __name__ == "__main__":
+    main()
